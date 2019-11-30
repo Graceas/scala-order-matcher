e38606758d82f0f56a29cdffa10fbf88edd9bc87
Graceas <grac.ga@gmail.com>
2019-11-30 15:25:00 +0700

    order matcher

---
+++ src/main/scala/ie/ordermatcher/OrderMatcher.scala
@@ -0,0 +1,149 @@
+package ie.ordermatcher
+
+import ie.ordermatcher.model.{Order, OrderBook, OrderBookHistory}
+import ie.ordermatcher.types.InstrumentType.InstrumentType
+import ie.ordermatcher.types.OrderType
+import ie.ordermatcher.types.OrderType.OrderType
+
+class OrderMatcher(
+  val instrument:       InstrumentType,
+  val orderBook:        OrderBook,
+  val orderBookHistory: OrderBookHistory
+) {
+
+  def start(): Unit = {
+    Boot.log(s"Execute $instrument instrument")
+
+    Boot.log(orderBook.sellOrders.toString())
+    Boot.log(orderBook.buyOrders.toString())
+
+    while (tick()) {
+      // execute all orders
+    }
+
+    Boot.log(orderBook.sellOrders.toString())
+    Boot.log(orderBook.buyOrders.toString())
+  }
+
+  def getAskPrice: Int = orderBook.sellOrders.head.price
+
+  def getBidPrice: Int = orderBook.buyOrders.head.price
+
+  def addOrder(order: Order): Boolean = {
+    order.orderType match {
+      case OrderType.BUY  => addBuyOrder(order)
+      case OrderType.SELL => addSellOrder(order)
+      case _              => false
+    }
+  }
+
+  private def tick(): Boolean = {
+
+    if (orderBook.sellOrders.isEmpty || orderBook.buyOrders.isEmpty) {
+      // sell or buy orders is not found
+      return false
+    }
+
+    Boot.log(s"$instrument: Ask=$getAskPrice, Bid=$getBidPrice")
+    trade(getEntry(OrderType.SELL))
+  }
+
+  private def trade(order: Order): Boolean = {
+
+    // get order from other pull
+    val otherOrder: Order = getEntry(order.orderType)
+    if (canTrade(order.orderType, order.price, otherOrder.price)) {
+      val delta: Int = order.volume - otherOrder.volume
+
+      if (delta < 0) { // sell order filled
+        // update volume for both orders
+
+        updateEntryVolume(order.orderType, 0)
+        updateEntryVolume(otherOrder.orderType, otherOrder.volume - order.volume)
+
+        removeEntry(order.orderType)
+      }
+
+      if (delta == 0) { // both orders filled
+        // update volume for both orders
+
+        updateEntryVolume(order.orderType, 0)
+        updateEntryVolume(otherOrder.orderType, 0)
+
+        removeEntry(order.orderType)
+        removeEntry(otherOrder.orderType)
+      }
+
+      if (delta > 0) { // buy orders filled
+        // update volume for both orders
+
+        updateEntryVolume(order.orderType, order.volume - otherOrder.volume)
+        updateEntryVolume(otherOrder.orderType, 0)
+
+        removeEntry(otherOrder.orderType)
+      }
+
+      true
+    } else {
+      false
+    }
+  }
+
+  private def canTrade(orderType: OrderType, orderPrice: Int, entryPrice: Int): Boolean =
+    if (orderType == OrderType.BUY)
+      orderPrice >= entryPrice
+    else
+      orderPrice <= entryPrice
+
+  private def getEntry(orderType: OrderType): Order =
+    if (orderType == OrderType.BUY)
+      orderBook.sellOrders.head
+    else
+      orderBook.buyOrders.head
+
+  private def updateEntryVolume(orderType: OrderType, volume: Int): Order =
+    if (orderType == OrderType.BUY) {
+      orderBook.buyOrders.update(0, orderBook.buyOrders.head.copy(volume = volume))
+      orderBook.buyOrders.head
+    } else {
+      orderBook.sellOrders.update(0, orderBook.sellOrders.head.copy(volume = volume))
+      orderBook.sellOrders.head
+    }
+
+  private def removeEntry(orderType: OrderType): Order =
+    if (orderType == OrderType.BUY)
+      orderBook.buyOrders.remove(0)
+    else
+      orderBook.sellOrders.remove(0)
+
+  private def addBuyOrder(order: Order): Boolean = {
+    if (
+      order.volume <= 0 ||
+      order.price <= 0  ||
+      order.instrument != instrument ||
+      order.client.balance < order.price * order.volume
+    ) {
+      false
+    } else {
+      orderBook.buyOrders.append(order)
+
+      true
+    }
+  }
+
+  private def addSellOrder(order: Order): Boolean = {
+    if (
+      order.volume <= 0 ||
+      order.price <= 0  ||
+      order.instrument != instrument ||
+      !order.client.instrumentBalances.contains(order.instrument) ||
+      order.client.instrumentBalances(order.instrument) < order.volume
+    ) {
+      false
+    } else {
+      orderBook.sellOrders.append(order)
+
+      true
+    }
+  }
+}
---
+++ src/test/scala/io/ordermatcher/OrderBookTest.scala
@@ -0,0 +1,299 @@
+package io.ordermatcher
+
+import ie.ordermatcher._
+import ie.ordermatcher.model.{Client, Order, OrderBook, OrderBookHistory}
+import ie.ordermatcher.types.{InstrumentType, OrderType}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class OrderBookTest extends AnyFlatSpec with Matchers {
+  "Order" should "be declined [BUY, user.balance < price * volume]" in {
+    val orderMatcher: OrderMatcher = new OrderMatcher(InstrumentType.A, OrderBook(), OrderBookHistory())
+    val client: Client = Client("A1", 10, Map.empty)
+    val order: Order = Order(
+      client     = client,
+      orderType  = OrderType.BUY,
+      price      = 100,
+      volume     = 5,
+      instrument = InstrumentType.A,
+      orderTime  = System.currentTimeMillis()
+    )
+
+    orderMatcher.addOrder(order) should be (false)
+  }
+
+  "Order" should "be declined [SELL, user.instrumentBalance < volume]" in {
+    val orderMatcher: OrderMatcher = new OrderMatcher(InstrumentType.A, OrderBook(), OrderBookHistory())
+    val client: Client = Client("A1", 10, Map.empty)
+    val order: Order = Order(
+      client     = client,
+      orderType  = OrderType.SELL,
+      price      = 100,
+      volume     = 5,
+      instrument = InstrumentType.A,
+      orderTime  = System.currentTimeMillis()
+    )
+
+    orderMatcher.addOrder(order) should be (false)
+  }
+
+  "Order" should "be declined [SELL, order.instrument <> matcher.instrument]" in {
+    val orderMatcher: OrderMatcher = new OrderMatcher(InstrumentType.A, OrderBook(), OrderBookHistory())
+    val client: Client = Client("A1", 10, Map(InstrumentType.B -> 999))
+    val order: Order = Order(
+      client     = client,
+      orderType  = OrderType.SELL,
+      price      = 100,
+      volume     = 5,
+      instrument = InstrumentType.B,
+      orderTime  = System.currentTimeMillis()
+    )
+
+    orderMatcher.addOrder(order) should be (false)
+  }
+
+  "Order" should "be accepted [BUY, user.balance >= price * volume]" in {
+    val orderMatcher: OrderMatcher = new OrderMatcher(InstrumentType.A, OrderBook(), OrderBookHistory())
+    val client: Client = Client("A1", 1000, Map.empty)
+    val order: Order = Order(
+      client     = client,
+      orderType  = OrderType.BUY,
+      price      = 100,
+      volume     = 5,
+      instrument = InstrumentType.A,
+      orderTime  = System.currentTimeMillis()
+    )
+
+    orderMatcher.addOrder(order) should be (true)
+  }
+
+  "Order" should "be declined [BUY, negative price]" in {
+    val orderMatcher: OrderMatcher = new OrderMatcher(InstrumentType.A, OrderBook(), OrderBookHistory())
+    val client: Client = Client("A1", 1000, Map.empty)
+    val order: Order = Order(
+      client     = client,
+      orderType  = OrderType.BUY,
+      price      = -100,
+      volume     = 5,
+      instrument = InstrumentType.A,
+      orderTime  = System.currentTimeMillis()
+    )
+
+    orderMatcher.addOrder(order) should be (false)
+  }
+
+  "Order" should "be declined [BUY, negative volume]" in {
+    val orderMatcher: OrderMatcher = new OrderMatcher(InstrumentType.A, OrderBook(), OrderBookHistory())
+    val client: Client = Client("A1", 1000, Map.empty)
+    val order: Order = Order(
+      client     = client,
+      orderType  = OrderType.BUY,
+      price      = 100,
+      volume     = -5,
+      instrument = InstrumentType.A,
+      orderTime  = System.currentTimeMillis()
+    )
+
+    orderMatcher.addOrder(order) should be (false)
+  }
+
+  "Order" should "be accepted [SELL, user.instrumentBalance >= volume]" in {
+    val orderMatcher: OrderMatcher = new OrderMatcher(InstrumentType.A, OrderBook(), OrderBookHistory())
+    val client: Client = Client("A1", 1000, Map(InstrumentType.A -> 10))
+    val order: Order = Order(
+      client     = client,
+      orderType  = OrderType.BUY,
+      price      = 100,
+      volume     = 5,
+      instrument = InstrumentType.A,
+      orderTime  = System.currentTimeMillis()
+    )
+
+    orderMatcher.addOrder(order) should be (true)
+  }
+
+  "Order" should "be accepted [SELL, negative price]" in {
+    val orderMatcher: OrderMatcher = new OrderMatcher(InstrumentType.A, OrderBook(), OrderBookHistory())
+    val client: Client = Client("A1", 1000, Map(InstrumentType.A -> 10))
+    val order: Order = Order(
+      client     = client,
+      orderType  = OrderType.BUY,
+      price      = -100,
+      volume     = 5,
+      instrument = InstrumentType.A,
+      orderTime  = System.currentTimeMillis()
+    )
+
+    orderMatcher.addOrder(order) should be (false)
+  }
+
+  "Order" should "be accepted [SELL, negative volume]" in {
+    val orderMatcher: OrderMatcher = new OrderMatcher(InstrumentType.A, OrderBook(), OrderBookHistory())
+    val client: Client = Client("A1", 1000, Map(InstrumentType.A -> 10))
+    val order: Order = Order(
+      client     = client,
+      orderType  = OrderType.BUY,
+      price      = 100,
+      volume     = -5,
+      instrument = InstrumentType.A,
+      orderTime  = System.currentTimeMillis()
+    )
+
+    orderMatcher.addOrder(order) should be (false)
+  }
+
+  "Orders" should "be full filled [price and volume equal]" in {
+    val orderMatcher: OrderMatcher = new OrderMatcher(InstrumentType.A, OrderBook(), OrderBookHistory())
+    val client:  Client = Client("A1", 1000, Map.empty)
+    val client2: Client = Client("A2", 1000, Map(InstrumentType.A -> 10))
+
+    val order: Order = Order(
+      client     = client,
+      orderType  = OrderType.BUY,
+      price      = 100,
+      volume     = 5,
+      instrument = InstrumentType.A,
+      orderTime  = System.currentTimeMillis()
+    )
+
+    val order2: Order = Order(
+      client     = client2,
+      orderType  = OrderType.SELL,
+      price      = 100,
+      volume     = 5,
+      instrument = InstrumentType.A,
+      orderTime  = System.currentTimeMillis()
+    )
+
+    orderMatcher.addOrder(order) should be (true)
+    orderMatcher.addOrder(order2) should be (true)
+
+    orderMatcher.start()
+
+    orderMatcher.orderBook.sellOrders should be (empty)
+    orderMatcher.orderBook.buyOrders should be (empty)
+  }
+
+  "Orders" should "be full filled [price and volume equal (two SELL and one BUY)]" in {
+    val orderMatcher: OrderMatcher = new OrderMatcher(InstrumentType.A, OrderBook(), OrderBookHistory())
+    val client:  Client = Client("A1", 1000, Map.empty)
+    val client2: Client = Client("A2", 1000, Map(InstrumentType.A -> 10))
+    val client3: Client = Client("A3", 1000, Map(InstrumentType.A -> 10))
+
+    val order: Order = Order(
+      client     = client,
+      orderType  = OrderType.BUY,
+      price      = 100,
+      volume     = 10,
+      instrument = InstrumentType.A,
+      orderTime  = System.currentTimeMillis()
+    )
+
+    val order2: Order = Order(
+      client     = client2,
+      orderType  = OrderType.SELL,
+      price      = 100,
+      volume     = 5,
+      instrument = InstrumentType.A,
+      orderTime  = System.currentTimeMillis()
+    )
+
+    val order3: Order = Order(
+      client     = client3,
+      orderType  = OrderType.SELL,
+      price      = 100,
+      volume     = 5,
+      instrument = InstrumentType.A,
+      orderTime  = System.currentTimeMillis()
+    )
+
+    orderMatcher.addOrder(order) should be (true)
+    orderMatcher.addOrder(order2) should be (true)
+    orderMatcher.addOrder(order3) should be (true)
+
+    orderMatcher.start()
+
+    orderMatcher.orderBook.sellOrders should be (empty)
+    orderMatcher.orderBook.buyOrders should be (empty)
+  }
+
+  "Orders" should "be full filled [price and volume equal (two SELL and five BUY)]" in {
+    val orderMatcher: OrderMatcher = new OrderMatcher(InstrumentType.A, OrderBook(), OrderBookHistory())
+    val clients: Map[String, Client] = Map(
+      // buy
+      "B1" -> Client("B1", 1000, Map.empty),
+      "B2" -> Client("B2", 1000, Map.empty),
+      "B3" -> Client("B3", 1000, Map.empty),
+      "B4" -> Client("B4", 1000, Map.empty),
+      "B5" -> Client("B5", 1000, Map.empty),
+      // sell
+      "S1" -> Client("S1", 0, Map(InstrumentType.A -> 15)),
+      "S2" -> Client("S2", 0, Map(InstrumentType.A -> 10))
+    )
+
+    val orders: Map[String, Order] = Map(
+      // buy
+      "B1" -> Order(client = clients("B1"), orderType = OrderType.BUY, price = 100, volume = 5, instrument = InstrumentType.A),
+      "B2" -> Order(client = clients("B2"), orderType = OrderType.BUY, price = 100, volume = 5, instrument = InstrumentType.A),
+      "B3" -> Order(client = clients("B3"), orderType = OrderType.BUY, price = 100, volume = 5, instrument = InstrumentType.A),
+      "B4" -> Order(client = clients("B4"), orderType = OrderType.BUY, price = 100, volume = 5, instrument = InstrumentType.A),
+      "B5" -> Order(client = clients("B5"), orderType = OrderType.BUY, price = 100, volume = 5, instrument = InstrumentType.A),
+      // sell
+      "S1" -> Order(client = clients("S1"), orderType = OrderType.SELL, price = 100, volume = 15, instrument = InstrumentType.A),
+      "S2" -> Order(client = clients("S2"), orderType = OrderType.SELL, price = 100, volume = 10, instrument = InstrumentType.A)
+    )
+
+    orderMatcher.addOrder(orders("B1")) should be (true)
+    orderMatcher.addOrder(orders("B2")) should be (true)
+    orderMatcher.addOrder(orders("B3")) should be (true)
+    orderMatcher.addOrder(orders("B4")) should be (true)
+    orderMatcher.addOrder(orders("B5")) should be (true)
+    orderMatcher.addOrder(orders("S1")) should be (true)
+    orderMatcher.addOrder(orders("S2")) should be (true)
+
+    orderMatcher.start()
+
+    orderMatcher.orderBook.sellOrders should be (empty)
+    orderMatcher.orderBook.buyOrders should be (empty)
+  }
+
+  "Orders" should "be part filled [price equal, SELL volume greater]" in {
+    val orderMatcher: OrderMatcher = new OrderMatcher(InstrumentType.A, OrderBook(), OrderBookHistory())
+    val clients: Map[String, Client] = Map(
+      // buy
+      "B1" -> Client("B1", 1000, Map.empty),
+      "B2" -> Client("B2", 1000, Map.empty),
+      "B3" -> Client("B3", 1000, Map.empty),
+      "B4" -> Client("B4", 1000, Map.empty),
+      "B5" -> Client("B5", 1000, Map.empty),
+      // sell
+      "S1" -> Client("S1", 0, Map(InstrumentType.A -> 15)),
+      "S2" -> Client("S2", 0, Map(InstrumentType.A -> 30))
+    )
+
+    val orders: Map[String, Order] = Map(
+      // buy
+      "B1" -> Order(client = clients("B1"), orderType = OrderType.BUY, price = 100, volume = 5, instrument = InstrumentType.A),
+      "B2" -> Order(client = clients("B2"), orderType = OrderType.BUY, price = 100, volume = 5, instrument = InstrumentType.A),
+      "B3" -> Order(client = clients("B3"), orderType = OrderType.BUY, price = 100, volume = 5, instrument = InstrumentType.A),
+      "B4" -> Order(client = clients("B4"), orderType = OrderType.BUY, price = 100, volume = 5, instrument = InstrumentType.A),
+      "B5" -> Order(client = clients("B5"), orderType = OrderType.BUY, price = 100, volume = 5, instrument = InstrumentType.A),
+      // sell
+      "S1" -> Order(client = clients("S1"), orderType = OrderType.SELL, price = 100, volume = 15, instrument = InstrumentType.A),
+      "S2" -> Order(client = clients("S2"), orderType = OrderType.SELL, price = 100, volume = 30, instrument = InstrumentType.A)
+    )
+
+    orderMatcher.addOrder(orders("B1")) should be (true)
+    orderMatcher.addOrder(orders("B2")) should be (true)
+    orderMatcher.addOrder(orders("B3")) should be (true)
+    orderMatcher.addOrder(orders("B4")) should be (true)
+    orderMatcher.addOrder(orders("B5")) should be (true)
+    orderMatcher.addOrder(orders("S1")) should be (true)
+    orderMatcher.addOrder(orders("S2")) should be (true)
+
+    orderMatcher.start()
+
+    orderMatcher.orderBook.sellOrders shouldNot be (empty)
+    orderMatcher.orderBook.buyOrders should be (empty)
+  }
+}

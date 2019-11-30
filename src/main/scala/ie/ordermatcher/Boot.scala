85aba817e05a7a1b4d4853a578e3e7b52ce8b600
Graceas <grac.ga@gmail.com>
2019-11-30 20:47:56 +0700

    execute orders from input source and save results

--- src/main/scala/ie/ordermatcher/Boot.scala
+++ src/main/scala/ie/ordermatcher/Boot.scala
@@ -1,7 +1,14 @@
 package ie.ordermatcher
 
+import java.io.{BufferedWriter, File, FileWriter}
+
 import ie.ordermatcher.helper.DataParser
-import ie.ordermatcher.model.{Client, Order}
+import ie.ordermatcher.model.{Client, Order, OrderBook, OrderBookHistory}
+import ie.ordermatcher.types.InstrumentType
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
 
 object Boot extends App {
 
@@ -10,7 +17,40 @@ object Boot extends App {
     val clients: List[Client] = DataParser.clientsParse("data/clients.txt")
     val orders:  List[Order]  = DataParser.ordersParse("data/orders.txt", clients)
 
-    println(clients, orders)
+    // initialize order matcher by instrument
+    val orderMatchers: List[OrderMatcher] = InstrumentType.values.toList.map(instrument =>
+      new OrderMatcher(instrument, OrderBook(), OrderBookHistory())
+    )
+
+    // add orders to order books
+    orderMatchers.foreach(orderMatcher => {
+      orders
+        .filter(order => order.instrument == orderMatcher.instrument)
+        .foreach(order => {
+          val status = if (orderMatcher.addOrder(order)) "accepted" else "declined"
+          log(s"${order.orderType} order by client ${order.client.name} is $status")
+        })
+    })
+
+    // run all matchers async
+    val futures = orderMatchers.map(orderMatcher => Future {
+      orderMatcher.start()
+    })
+
+    Await.result(Future.sequence(futures), Duration.Inf)
+
+    // cancel all orders for release clients funds
+    orderMatchers.foreach(orderMatcher => orderMatcher.cancelAllOrders())
+
+    // save results to file
+    val file = new File("result.txt")
+    val bw = new BufferedWriter(new FileWriter(file))
+    bw.write(clients.map(client => {
+      s"${client.name}\t${client.balance}\t${client.instrumentBalances(InstrumentType.A)}\t" +
+      s"${client.instrumentBalances(InstrumentType.B)}\t${client.instrumentBalances(InstrumentType.C)}\t" +
+      s"${client.instrumentBalances(InstrumentType.D)}"
+    }).mkString("\n"))
+    bw.close()
   }
 
   def log(str: String): Unit = println(s"${System.currentTimeMillis()}: $str")
